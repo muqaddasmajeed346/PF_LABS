23bb8c8fe0137dea33428d6f0ff994451e3bcf78
muqaddasmajeed346 <muqaddasmajeed674@gmail.com>
2025-12-22 16:40:35 -0800

    pf-labs

---
+++ pf_lab_9/pf task 9 lab 1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   	int i=1;
+   	int N;
+   	cout<<"enter ineger"<<endl;
+   	cin>>N;
+   	while(i <= N)
+   	{
+   		cout<<i<<endl;
+   		i++;
+	   }
+	   return 0;
+}
