7d3aa43fea90c4051cd7b58794859351a5400a1d
muqaddasmajeed346 <muqaddasmajeed674@gmail.com>
2026-02-10 10:08:23 -0800

    PF labs
    basic c++

---
+++ lab 6 and 7/PF LAB_6 TASK 1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num;
+	cout<<"enter a number"<<endl;
+	cin>>num;
+	if(num>0){
+	cout<<"the umber is positive"<<endl;
+}
+    else if(num<0){
+	cout<<"The number is negative"<<endl;	 
+  } 
+  else{
+  cout<<"num = 0"<<endl;
+}
+return 0;
+}
